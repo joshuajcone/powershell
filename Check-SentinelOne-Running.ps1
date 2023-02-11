7588c753f178e549069c6d8760741870723dc87a
ConeyIsland <56121645+joshuajcone@users.noreply.github.com>
2023-02-10 20:31:22 -0600

    Update Check-SentinelOne-Running.ps1
    Added timer, and additional check for disk scan.

--- Check-SentinelOne-Running.ps1
+++ Check-SentinelOne-Running.ps1
@@ -1,28 +1,46 @@
 # Base Sentinel Directory
-$directory = "C:\Program Files\SentinelOne\"
+$basePath = "C:\Program Files\SentinelOne\"
 
 # Gets the folder based on the version of Sentinel installed
-$sentinelFolder = Get-ChildItem -Path $directory -Directory
+$sentinelDirectory = Get-ChildItem -Path $basePath -Directory
 
 # Sets the Path Name from Folder Name
-$sentinelName = $sentinelFolder.Name
+$sentinelName = $sentinelDirectory.Name
 
 # Sets Full directory
-$sentinelpath = "$directory$sentinelName"
+$sentinelpath = "$basePath$sentinelName"
 
 # Sets the location to run the command
 Set-Location $sentinelPath
 
-# Command to Check Status
-$sentinelStatus = & ".\SentinelCtl.exe" read_fdcs_status
+# Command to Check Status FDCS status
+$FDCSStatus = & ".\SentinelCtl.exe" read_fdcs_status
+
+#Comman to Check if Scan is Running
+$ScanStatus = & ".\SentinelCtl.exe" is_scan_in_progress
 
 # If Status is not 2 it will continue to check
-if ($sentinelStatus -ne "FDCS Status: 2") {
+if ($FDCSStatus -ne "FDCS Status: 2") {
+    $timer = [Diagnostics.Stopwatch]::StartNew()
     do {
         Write-Host "Sentinel Scan is Running"
         Start-Sleep -Seconds 60
-    } until (
-        $sentinelStatus -eq "FDCS Status: 2"
-    )   
-} 
+        $FDCSStatus = & ".\SentinelCtl.exe" read_fdcs_status
+    } until ($FDCSStatus -eq "FDCS Status: 2")
+}
+if ($ScanStatus -ne "Scan is not in progress") {
+    $timer = [Diagnostics.Stopwatch]::StartNew()
+    do {
+        Write-Host "Sentinel Scan is Running"
+        Start-Sleep -Seconds 60
+        $ScanStatus = & ".\SentinelCtl.exe" is_scan_in_progress
+    } until ($ScanStatus -eq "Scan is not in progress")
+}
 Write-Host "Sentinel Scan is Complete"
+if ($null -ne $timer) {
+    $timer.Stop()
+    $hours = $timer.elapsed.hours
+    $minutes = $timer.elapsed.minutes
+    $seconds = $timer.elapsed.seconds
+    Write-Host "Sentinel Scan took $hours hours, $minutes minutes, $seconds seconds"
+}
